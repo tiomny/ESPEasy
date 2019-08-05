e322f0b77ded88a8b20e206740260d7cf46ad4b3
Artem <tiomny@yandex.ru>
2019-08-05 21:31:45 +0300

    ADC calibration Fix

--- src/_P002_ADC.ino
+++ src/_P002_ADC.ino
@@ -183,9 +183,9 @@ float P002_applyCalibration(struct EventStruct *event, float float_value) {
     float adc2 = static_cast<float>(PCONFIG_LONG(1));
     float out1 = PCONFIG_FLOAT(0);
     float out2 = PCONFIG_FLOAT(1);
-    if (adc1 != out1)
+    if (adc1 != adc2)
     {
-      float_value = (float_value - adc1) * (out2 - out1) / (out1 - adc1) + out1;
+      float_value = (float_value - adc1) * (out2 - out1) /(adc2 - adc1) + out1;
     }
   }
   return float_value;
